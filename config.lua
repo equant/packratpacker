134223fb414d551db269acb945787e92a32d9354
Nathanial Hendler <nathanhendler@gmail.com>
2016-12-08 09:06:11 -0700

    Realistic times for testing

--- config.lua
+++ config.lua
@@ -1,5 +1,5 @@
 state = "unknown"
-ap_check_cadence = 120
+ap_check_cadence = 1800
 
 gpio16 = 0  --- Don't use this if you want to use node.dsleep())
 gpio5  = 1
@@ -14,9 +14,9 @@ gpio15 = 8
 led_sequences = {
     -- PINS, Blink_length(us), Repetitions, Cadence (s)
     -- rgb_hood
-    {{gpio5, gpio4, gpio0}, 200, 40, 30},
+    {{gpio5, gpio4, gpio0}, 100, 10, 900},
     -- blinking_taillight
-    {{gpio2, gpio14}, 1000, 3, 30},
+    {{gpio2, gpio14}, 1000, 2, 901},
     -- vibrator
-    {{gpio12}, 1500, 1, 60}
+    {{gpio12}, 1100, 1, 3600}
 }
