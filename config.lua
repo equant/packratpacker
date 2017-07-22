61a5c965331a9c0872e5ee6e23327c2c215c09c8
Nathanial Hendler <nathanhendler@gmail.com>
2017-07-22 12:45:33 -0700

    amundsen w/ Tim, working version

--- config.lua
+++ config.lua
@@ -5,7 +5,7 @@ gpio16 = 0  --- Don't use this if you want to use node.dsleep())
 gpio5  = 1
 gpio4  = 2
 gpio0  = 3
-gpio2  = 4
+gpio2  = 4  --- This is the onboard LED For the D1 mini (Low: On)
 gpio14 = 5
 gpio12 = 6
 gpio13 = 7
@@ -16,9 +16,9 @@ led_sequences = {
     -- rgb_hood
     {{gpio5, gpio4, gpio0}, 35, 6, 15},
     -- blinking_taillight
-    {{gpio2, gpio14}, 1000, 2, 302},
+    --{{gpio2, gpio14}, 1000, 2, 302},
     -- vibrator -- 90 minutes
     {{gpio12}, 1100, 1, 33},
     -- rgb_hood reversed
-    {{gpio0, gpio4, gpio5}, 35, 6, 15},
+    --{{gpio0, gpio4, gpio5}, 35, 6, 15},
 }
